e935993fdddfe8f3d13606b319c803bc1152ecb2
Levan Kaniashvili <levanikaniashvili@yahoo.com>
2026-02-23 22:34:02 +0400

    warehouse app was updated

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,3 +1,4 @@
+#warehouse/urls.py
 from django.urls import path
 from . import views
 
@@ -5,4 +6,5 @@ app_name = "warehouse"
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
+    path("download-excel/", views.download_excel, name="download_excel"),
 ]
